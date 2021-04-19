ae4c0a59023a75be0e2372eb98c96595ea00c3ab
Jamie Barnes <jlb211@uclive.ac.nz>
2021-04-19 23:24:35 +1200

    Added Javadoc Comments and adjusted the Island class
    Added Javadoc Comments for Island and Item classes.
    Added addRoute method to Island class and refined trades variable.

--- src/Item.java
+++ src/Item.java
@@ -3,15 +3,29 @@ public class Item {
 	private String name;
 	private int weight;
 	
+	
+	/**
+	 * Constructs an Item with the given Name and Weight.
+	 * @param name the name of the Item
+	 * @param weight the weight of the Item
+	 */
 	public Item(String name, int weight) {
 		this.name = name;
 		this.weight = weight;
 	}
 	
+	/**
+	 * Returns the name of the Item
+	 * @return the name of the Item
+	 */
 	public String getName() {
 		return this.name;
 	}
 	
+	/**
+	 * Returns the weight of the Item
+	 * @return the weight of the Item
+	 */
 	public int getWeight() {
 		return this.weight;
 	}
