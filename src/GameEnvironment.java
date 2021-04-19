6d95c1c62aba9270a56303e97f1e194cb4d1ff26
Ehst-SG <sethgunn18@gmail.com>
2021-04-19 23:13:59 +1200

    Adding Descriptions
    Adding descriptions to the Route and Ship class files as well as initialising the ships in the GameEnvironment class

--- src/GameEnvironment.java
+++ src/GameEnvironment.java
@@ -1,6 +1,8 @@
-
 public class GameEnvironment {
 	public static void main(String[] args) {
-		
+		Ship saintMary = new Ship("Saint Mary", 30, 40, 90);
+		Ship perlaBetty = new Ship("Perla Betty III", 80, 60, 40);
+		Ship goldenHand = new Ship("The Golden Hand", 60, 55, 60);
+		Ship artemis = new Ship("Artemis", 40, 70, 30;)
 	}
 }
