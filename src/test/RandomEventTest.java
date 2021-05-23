d12d9bc2d644745db310c298e48042cd68ebb9dd
Ehst-SG <sethgunn18@gmail.com>
2021-05-23 16:04:32 +1200

    Update RandomEventTest.java

--- src/test/RandomEventTest.java
+++ src/test/RandomEventTest.java
@@ -20,6 +20,13 @@ class RandomEventTest {
 	}
 	
 	@Test
+	void shipDeathTest() {
+		Ship testShip = new Ship("Test Ship", 80, 60, 30);
+		assertEquals("The storm has completely destroyed your ship and all of its cargo.", testEvent.tryEvent(testShip, 0.4, 1, 0.5).get(1));
+
+	}
+	
+	@Test
 	void rescueTest() {
 		assertEquals("You spot some drowning sailors and decide to rescue them.\nThey give you some gold as thanks.", testEvent.tryEvent(testShip, 0.4, 1, 1).get(0));
 	}
