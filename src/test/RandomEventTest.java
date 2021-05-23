dabb1421f1f1b92364d42eb1427f3ab57b51d673
Ehst-SG <sethgunn18@gmail.com>
2021-05-23 16:26:45 +1200

    Added more tests for RandomEvent

--- src/test/RandomEventTest.java
+++ src/test/RandomEventTest.java
@@ -14,6 +14,29 @@ class RandomEventTest {
 	@Test
 	void pirateAttackTest() {
 		assertEquals("You have encountered pirates!", RandomEvent.tryEvent(testShip, 1.0, 0, 0.2).get(0));
+		
+	}
+	
+	@Test
+	void pirateEscapeTest() {
+		Ship testShip = new Ship("Test Ship", 80, 6000, 40);
+		assertEquals("You escaped the enemy pirates!", RandomEvent.tryEvent(testShip, 1.0, 0, 0.2).get(1));
+	}
+	
+	@Test
+	void pirateDeathTest() {
+		Ship testShip = new Ship("Test Ship", 800, 60, 40);
+		assertEquals("The pirates are unhappy with your cargo. You have been made to walk the plank.", RandomEvent.tryEvent(testShip, 1.0, 0, 0.2).get(2));
+	}
+	
+	@Test
+	void pirateTakeCargoTest() {
+		Ship testShip = new Ship("Test Ship", 800, 60, 40);
+		Item testItem = new Item("", 0, 1000);
+		testShip.initialiseCargo(testItem);
+		testShip.addItemCargo(testItem, 5);
+		assertEquals("The pirates have taken all of your cargo and mercifully let you and your crew live.", RandomEvent.tryEvent(testShip, 1.0, 0, 0.2).get(2));
+		
 	}
 	
 	@Test
@@ -28,8 +51,11 @@ class RandomEventTest {
 	
 	@Test
 	void shipDeathTest() {
-		Ship testShip = new Ship("Test Ship", 80, 60, 30);
-		assertEquals("The storm has completely destroyed your ship and all of its cargo.", testEvent.tryEvent(testShip, 0.4, 1, 0.5).get(1));
+		Ship testShip = new Ship("Test Ship", 80, 60, 5);
+		for (Item item: GameEnvironment.getItems()) {
+			testShip.initialiseCargo(item);
+		}
+		assertEquals("The storm has completely destroyed your ship and all of its cargo.", RandomEvent.tryEvent(testShip, 1, 0, 0.5).get(1));
 
 	}
 	
